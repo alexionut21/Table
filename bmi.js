6ea9afe005db46d2a40388bad16f82a13116168f
alexionut21 <mocanuionutalex21@gmail.com>
2018-04-04 17:17:50 +0300

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,26 @@
+
+function calcalcIt(){
+/*
+var weight = document.calc.weight.value
+var height = document.calc.cm.value
+var inches = document.calc.inches.value
+var feet = document.calc.feet.value
+if(weight > 0 && height > 0){	
+var finalBmi = weight/(height/100*height/100)
+var finalBmiPounds = (weight/(height/100*height/100))*703
+get("answer").innerHTML = finalBmi;
+if(finalBmi < 18.5){
+get("lose").innerHTML = "That you are too thin."
+}
+if(finalBmi > 18.5 && finalBmi < 25){
+get("lose").innerHTML = "That you are healthy."
+}
+if(finalBmi > 25){
+get("lose").innerHTML = "That you have overweight."
+}
+}
+else{
+alert("Please Fill in everything correctly")
+}*/
+    get("printArea").style.display = "block", smoothScroll(document.getElementById("Button1"), 500);
+}
