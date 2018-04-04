a2f8297a689e5d4b83b73e7eb274aacfcc750ec0
alexionut21 <mocanuionutalex21@gmail.com>
2018-04-04 23:32:15 +0300

    Add files via upload

--- bmi.js
+++ bmi.js
@@ -1,26 +1,41 @@
 
 function calcalcIt(){
-/*
+
 var weight = document.calc.weight.value
 var height = document.calc.cm.value
 var inches = document.calc.inches.value
 var feet = document.calc.feet.value
 if(weight > 0 && height > 0){	
 var finalBmi = weight/(height/100*height/100)
-var finalBmiPounds = (weight/(height/100*height/100))*703
-get("answer").innerHTML = finalBmi;
+document.getElementById("answer").innerHTML = Math.round(finalBmi);
 if(finalBmi < 18.5){
-get("lose").innerHTML = "That you are too thin."
+document.getElementById("lose").innerHTML = "That you are too thin."
 }
 if(finalBmi > 18.5 && finalBmi < 25){
-get("lose").innerHTML = "That you are healthy."
+document.getElementById("lose").innerHTML = "That you are healthy."
 }
 if(finalBmi > 25){
-get("lose").innerHTML = "That you have overweight."
+document.getElementById("lose").innerHTML = "That you have overweight."
 }
+document.getElementById("printArea").style.display="block";
+}
+else if(weight > 0 && (feet > 0 || inches > 0)){	
+var d=parseInt(feet*12)+parseInt(inches);
+var finalBmi = (weight/(d*d))*703
+document.getElementById("answer").innerHTML = Math.round(finalBmi);
+if(finalBmi < 18.5){
+document.getElementById("lose").innerHTML = "That you are too thin."
+}
+if(finalBmi > 18.5 && finalBmi < 25){
+document.getElementById("lose").innerHTML = "That you are healthy."
+}
+if(finalBmi > 25){
+document.getElementById("lose").innerHTML = "That you have overweight."
+}
+document.getElementById("printArea").style.display="block";
 }
 else{
 alert("Please Fill in everything correctly")
-}*/
-    get("printArea").style.display = "block", smoothScroll(document.getElementById("Button1"), 500);
+}
+	
 }
