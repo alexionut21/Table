bd0d291f94b8a48c7955d663d998796e4b0c4210
alexionut21 <mocanuionutalex21@gmail.com>
2018-04-11 16:30:56 +0300

    Add files via upload

---
+++ AskQcomments.js
@@ -0,0 +1,74 @@
+
+	<!--Coment Section-->
+p {
+  line-height: 2.3125rem;
+}
+
+.comments {
+  margin-top: 15%;
+  margin-left: 1%;
+  width: 80.75rem;
+}
+
+.comment-wrap {
+  margin-bottom: 1.25rem;
+  display: table;
+  width: 100%;
+  min-height: 5.3125rem;
+}
+
+.photo {
+  padding-top: 0.825rem;
+  display: table-cell;
+  width: 6.5rem;
+}
+.photo .avatar {
+  height: 5.25rem;
+  width: 5.25rem;
+  border-radius: 45%;
+  background-size: contain;
+}
+
+.comment-block {
+  padding: 3rem;
+  background-color: #fff;
+  display: table-cell;
+  vertical-align: top;
+  border-radius: 0.1875rem;
+  box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.09);
+}
+.comment-block textarea {
+  width: 100%;
+  resize: none;
+}
+
+.comment-text {
+  margin-bottom: 1.25rem;
+  font-size: 1.5rem;
+}
+
+.bottom-comment {
+  color: #acb4c2;
+  font-size: 1.45rem;
+}
+
+.comment-date {
+  float: left;
+}
+
+.comment-actions {
+  float: right;
+}
+.comment-actions li {
+  display: inline;
+  margin: -2px;
+  cursor: pointer;
+}
+.comment-actions li.reply {
+  padding-left: 0.75rem;
+  padding-right: 0.125rem;
+}
+.comment-actions li:hover {
+  color: #0095ff;
+}
+
